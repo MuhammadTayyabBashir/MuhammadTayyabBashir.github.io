e35cd19fbd376abd632c9c796d7f91aca60246b7
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-09-25 22:43:44 +0500

    Deploy React build to main

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,9 @@
-// export default defineConfig({
-//   server: {
-//     port: 5173, // or your frontend port
-//     hmr: {
-//       protocol: 'ws',
-//       host: 'localhost',
-//       port: 5173, 
-//     },
-//     proxy: {
-//       '/api': 'http://localhost:10000',
-//     },
-//   },
-// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', 
 });
